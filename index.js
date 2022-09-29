982473c7e1c50c90903fbb6d1a01019c38be5e44
vlasmar <vlasopoulou.m@gmail.com>
2022-09-29 10:36:34 +0200

    add GET / routes for id and info

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const data = require('./data.json')
-const app = express()
-const port = 3000
+const express = require('express');
+const data = require('./data.json');
+const app = express();
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello world!');
+})
+
+app.get('/pokemon/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const pokemon = data.find((pokemon) => {
+        return pokemon.id === id;
+    });
+    res.json(pokemon);
+})
+
+app.get('/pokemon/:id/:info', (req, res) => {
+  const id = Number(req.params.id);
+  const info = req.params.info;
+  const pokemonInfo = data.find((pokemon) => {
+    return pokemon.id === id;
+  })
+  if (info === 'name'){
+    res.send(pokemonInfo.name);
+  } else if (info === 'type'){
+    res.send(pokemonInfo.type);
+  } else if (info === 'base'){
+    res.send(pokemonInfo.base);
+  }
 })
 
 app.listen(port, () => {
